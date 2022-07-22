0f545a169781627918ef3639bf614d245a640b12
Johnson Duong <71087545+johnsonduong@users.noreply.github.com>
2022-07-22 13:29:23 -0400

    Create skeleton and handle routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Experience from "./pages/Experience";
+import Education from "./pages/Education";
+import Contact from "./pages/Contact";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">Hello</header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/experience" element={<Experience />} />
+        <Route path="/education" element={<Education />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Container from "@mui/material/Container";
+
+const Home = () => {
+  return <div>Home</div>;
+};
+
+export default Home;
