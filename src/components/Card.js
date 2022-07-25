eb9d0208333aa5ec20b87cdb9197de33f7e29c0e
Johnson Duong <71087545+johnsonduong@users.noreply.github.com>
2022-07-25 14:09:15 -0400

    Fix left shifting when scrollbar appears

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,23 +7,25 @@ import { styled } from "@mui/material/styles";
 
 const Card = (props) => {
   return (
-    <Box backgroundColor={props.backgroundColor} sx={{ flexGrow: 1, height: "60vh" }}>
-      <Container maxWidth="lg" sx={{ display: "flex", flexDirection: "row", flexFlow: 1, justifyContent: "flex-start", p: 10 }}>
-        <Box sx={{ width: "50%", mr: 15 }}>
-          <Typography variant="h3" color="initial" sx={{ mb: 2 }}>
+    <Box backgroundColor={props.backgroundColor}>
+      <Container maxWidth="lg" sx={{ display: "flex", alignItems: "center", height: "400px" }}>
+        <Box sx={{ width: "70%", py: 5 }}>
+          <Typography variant="h3" color="initial" sx={{ mb: 1 }}>
             {props.title}
           </Typography>
           <Typography variant="h5" color="initial">
             {props.heading}
           </Typography>
-          <Typography variant="body1" color="initial" sx={{ mb: 5 }}>
+          <Typography variant="body1" color="initial" sx={{ mb: 3 }}>
             {props.date}
           </Typography>
           <Typography variant="body1" color="initial">
             {props.info}
           </Typography>
         </Box>
-        <img src={require("../images/profile.png")} alt="" style={{ width: "20vw" }} />
+        <Box>
+          <img src={require("../images/profile.png")} alt="" style={{ width: "200px" }} />
+        </Box>
       </Container>
     </Box>
   );
