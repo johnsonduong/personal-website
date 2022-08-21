9fe12984eaa166df2d56142d7960f96dd6738686
Johnson Duong <71087545+johnsonduong@users.noreply.github.com>
2022-08-21 17:56:07 -0400

    Enable cursor change on project card hover

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -20,55 +20,59 @@ const ProjectCard = (props) => {
       maxWidth="lg"
       onClick={handleNav}
       disableGutters
-      sx={{ display: "flex", alignItems: "center", minHeight: "400px", mb: 5, p: 0, backgroundColor: "white", boxShadow: "0px 3px 20px #616161" }}
+      sx={{ display: "flex", flexGrow: 1, justifyContent: "center", alignItems: "center", minHeight: "400px", mb: 5, p: 0, boxShadow: "0px 3px 20px #616161" }}
       onMouseOver={() => setShowInfo(true)}
       onMouseOut={() => setShowInfo(false)}
     >
-      <Box sx={{ py: 5, pl: 8, flexGrow: 1 }}>
-        <Typography variant="h3" color="inherit" sx={{ fontWeight: "bold", fontSize: { xs: 30, sm: 36, md: 36, lg: 40, xl: 40 } }}>
-          {props.title}
-        </Typography>
-        {showInfo && (
-          <Typography variant="h5" color="inherit" sx={{ fontSize: { xs: 18, sm: 18, md: 20, lg: 22, xl: 22 } }}>
-            {props.heading}
-          </Typography>
-        )}
+      <a style={{ display: "inline-block", width: "100%", backgroundColor: "white" }}>
+        <Box sx={{ display: "flex", alignItems: "center", justifyContent: "space-between", width: "100%", flexGrow: 1 }}>
+          <Box sx={{ py: 5, pl: 8 }}>
+            <Typography variant="h3" color="inherit" sx={{ fontWeight: "bold", fontSize: { xs: 30, sm: 36, md: 36, lg: 40, xl: 40 } }}>
+              {props.title}
+            </Typography>
+            {showInfo && (
+              <Typography variant="h5" color="inherit" sx={{ fontSize: { xs: 18, sm: 18, md: 20, lg: 22, xl: 22 } }}>
+                {props.heading}
+              </Typography>
+            )}
 
-        {showInfo ? (
-          <Button
-            onClick={handleNav}
-            sx={{
-              mt: 2,
-              px: 2,
-              borderRadius: 0,
-              border: "solid black 3px",
-              fontWeight: "bold",
-              boxShadow: "none",
-              backgroundColor: "black",
-              color: "white",
-            }}
-          >
-            Explore
-          </Button>
-        ) : (
-          <Button
-            onClick={handleNav}
-            sx={{
-              mt: 2,
-              px: 2,
-              borderRadius: 0,
-              border: "solid black 3px",
-              fontWeight: "bold",
-              boxShadow: "none",
-              color: "black",
-              backgroundColor: "white",
-            }}
-          >
-            Explore
-          </Button>
-        )}
-      </Box>
-      <Box sx={{ width: 550, height: 400, backgroundImage: `url(${props.images[0]})`, backgroundSize: "cover", backgroundPosition: "center" }}></Box>
+            {showInfo ? (
+              <Button
+                onClick={handleNav}
+                sx={{
+                  mt: 2,
+                  px: 2,
+                  borderRadius: 0,
+                  border: "solid black 3px",
+                  fontWeight: "bold",
+                  boxShadow: "none",
+                  backgroundColor: "black",
+                  color: "white",
+                }}
+              >
+                Explore
+              </Button>
+            ) : (
+              <Button
+                onClick={handleNav}
+                sx={{
+                  mt: 2,
+                  px: 2,
+                  borderRadius: 0,
+                  border: "solid black 3px",
+                  fontWeight: "bold",
+                  boxShadow: "none",
+                  color: "black",
+                  backgroundColor: "white",
+                }}
+              >
+                Explore
+              </Button>
+            )}
+          </Box>
+          <Box sx={{ width: 550, height: 400, backgroundImage: `url(${props.images[0]})`, backgroundSize: "cover", backgroundPosition: "center" }}></Box>
+        </Box>
+      </a>
     </Container>
   );
 };
